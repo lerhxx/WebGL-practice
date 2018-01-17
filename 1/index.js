07bf5730818cc9ea55af6b202684133173c4e0df
lerhxx <407785526@qq.com>
2018-01-17 18:09:45 +0800

    change color

--- 1/index.js
+++ 1/index.js
@@ -1,10 +1,16 @@
 ;let gl = null
+
 window.onload  = () => {
-    main()
+    main('canvas')
 }
 
-function main () {
-    gl = getWebGLContext('canvas')
+function main (id) {
+    const canavs = document.getElementById(id)
+    if (!canvas) {
+        return null
+    }
+
+    gl = getWebGLContext(canavs)
     if (!gl) {
         return
     }
@@ -15,23 +21,21 @@ function main () {
     const program = createProgram('vs', 'fs')
 
     const pos_loc = gl.getAttribLocation(program, 'position')
-    const buffer = createBuffer([
-        0, 0, 0
-    ])
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+    const pos_buffer = createBuffer()
+
     gl.enableVertexAttribArray(pos_loc)
+
     gl.vertexAttribPointer(pos_loc, 3, gl.FLOAT, false, 0, 0)
 
-    gl.drawArrays(gl.POINTS, 0, 1)
+    const size_loc = gl.getAttribLocation(program, 'a_PointSize')
+    gl.vertexAttrib1f(size_loc, 20.0)
+
+    bindClickEvent(canvas, pos_buffer)
     gl.flush()
 }
 
-function getWebGLContext (id) {
-    const canavs = document.getElementById(id)
-    if (!canvas) {
-        return null
-    }
+function getWebGLContext (canavs) {
     canvas.width = '500'
     canvas.height = '500'
 
@@ -91,11 +95,41 @@ function createProgram (vsId, fsId) {
     }
 }
 
-function createBuffer (data) {
+function createBuffer (data = []) {
     const buffer = gl.createBuffer()
 
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW)
 
     return buffer
+}
+
+function bindClickEvent (ele, pos_buffer) {
+    const rect = ele.getBoundingClientRect()
+    const hw = rect.width / 2
+    const hh = rect.height / 2
+
+    const points = []
+    const colors = []
+
+    ele.addEventListener('mousedown', e => {
+        const x = e.clientX
+        const y = e.clientY
+        
+        const rx = (x - rect.left - hw) / hw
+        const ry = (hh -y - rect.top) / hh
+        
+        points.push(rx)
+        points.push(ry)
+        points.push(0)
+
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW)
+
+        gl.clear(gl.COLOR_BUFFER_BIT)
+        gl.drawArrays(gl.POINTS, 0, points.length / 3)
+    })
+}
+
+function random (min = 0, max = 1) {
+    return Math.random() * ( max - min ) + min
 }
